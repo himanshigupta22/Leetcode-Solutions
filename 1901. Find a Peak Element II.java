3216d0ec7f6aa376266d89cc518db7a5b9d05254
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2024-10-20 15:39:08 +0530

    Create 1901. Find a Peak Element II.java

---
+++ 1901. Find a Peak Element II.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxElement(int[][] mat, int n, int m, int col){
+        int maxValue = -1, index = -1;
+        for(int i = 0; i < n; i++){
+            if(mat[i][col] > maxValue){
+                maxValue = mat[i][col];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public int[] findPeakGrid(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+        int low = 0, high = m - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int row = maxElement(mat, n, m, mid);
+            int left = mid - 1 >= 0 ? mat[row][mid-1] : -1;
+            int right = mid + 1 < m ? mat[row][mid+1] : -1;
+            if(mat[row][mid] > left && mat[row][mid] > right){
+                return new int[] {row, mid};
+            } else if(mat[row][mid] <  left){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
