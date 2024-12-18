b2aaebfd9e7b11c78bc827155316cb1065f01291
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2024-12-18 13:08:31 +0530

    Create 496. Next Greater Element I.java

---
+++ 496. Next Greater Element I.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = nums2.length-1; i >= 0; i--){
+            if(st.size() == 0){
+                mpp.put(nums2[i], -1);
+            } else if(st.size() > 0 && st.peek() > nums2[i]){
+                mpp.put(nums2[i], st.peek());
+            } else if(st.size() > 0 && st.peek() <= nums2[i]){
+
+                while(st.size() > 0 && st.peek() <= nums2[i]){
+                    st.pop();
+                } 
+                if(st.isEmpty()){
+                    mpp.put(nums2[i], -1);
+                } else {
+                    mpp.put(nums2[i], st.peek());
+                }
+            }
+            st.push(nums2[i]);
+        }
+    
+        int[] ans = new int[nums1.length];
+        for(int i = 0; i < nums1.length; i++){
+            ans[i] = mpp.get(nums1[i]);
+        }
+        
+        return ans;
+    }
+}
